57e15b5f2cb4d1deac2e17223de9ae0b6fe30e41
Yevgen Nesterenko <Yevgen_Nesterenko@epam.com>
2015-12-04 15:19:18 +0200

    fixes

--- js/app.js
+++ js/app.js
@@ -7,7 +7,10 @@
     app.controller('PanelController', function() {    
     this.tab = 1;
     this.selectTab = function(setTab)  {
-    	this.tab = setTab;
+    	this.tab = setTab;        
+    };
+    this.isSelected = function(checkTab){
+        return this.tab == checkTab;
     };
     });
     var gems = [
